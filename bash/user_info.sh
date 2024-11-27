72866523b87b82b1db3091f0665708cbcc3b9422
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2024-11-27 22:54:05 +0100

    bash

--- bash/user_info.sh
+++ bash/user_info.sh
@@ -1,10 +1,17 @@
 #/bin/bash
 
+# Check if a user argument was provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 username"
+    exit 1
+fi
+
+user="$1"
 user_info=$(getent passwd "$user")
 
 if [ -z "$user_info" ]; then
     echo "User $user not found."
-    return 1
+    exit 1
 fi
 
 groups=$(groups "$user" | cut -d: -f2- | sed 's/^ //' | tr ' ' ',')
