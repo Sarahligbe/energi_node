30d82879e7b6037823f118dbe3af5d12e35bd4a7
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2024-11-28 00:54:34 +0100

    update

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Fetch the external IP address
+# Fetch the external IP address. From https://github.com/energicryptocurrency/energi3-provisioning/blob/master/docker-compose/scripts/energi-core-run.sh
 EXT_IP=$(curl -s https://api.ipify.org)
 
 if [ -z "$EXT_IP" ]; then
@@ -8,7 +8,7 @@ if [ -z "$EXT_IP" ]; then
     exit 1
 fi
 
-# Execute the Energi node
+# Execute the Energi node. From https://wiki.energi.world/docs/guides/core-node-linux#22-setup-auto-start
 exec /home/energiuser/energi3/bin/energi3 \
     --datadir /home/energiuser/.energicore3 \
     --gcmode archive \
