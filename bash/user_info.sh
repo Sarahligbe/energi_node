bab18ffd86885b3223c40ad70ef7483016fe000b
Sarah Aligbe <37581442+Sarahligbe@users.noreply.github.com>
2024-11-27 22:47:40 +0100

    bash

--- bash/user_info.sh
+++ bash/user_info.sh
@@ -1,16 +1,16 @@
 #/bin/bash
 
-local user_info=$(getent passwd "$user")
+var user_info=$(getent passwd "$user")
 
 if [ -z "$user_info" ]; then
     echo "User $user not found."
     return 1
 fi
 
-local groups=$(groups "$user" | cut -d: -f2- | sed 's/^ //' | tr ' ' ',')
-local home_dir=$(echo "$user_info" | cut -d: -f6)
-local shell=$(echo "$user_info" | cut -d: -f7)
-local last_login=$(lastlog -u "$user" | awk 'NR==2 { 
+var groups=$(groups "$user" | cut -d: -f2- | sed 's/^ //' | tr ' ' ',')
+var home_dir=$(echo "$user_info" | cut -d: -f6)
+var shell=$(echo "$user_info" | cut -d: -f7)
+var last_login=$(lastlog -u "$user" | awk 'NR==2 { 
     if ($0 ~ /Never logged in/) {
         print "Never logged in"
     } else if (NF > 1) {
